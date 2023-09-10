23a65828d3139fc45228eabb1843d484890f5d5e
razewithlove <mister.timkinbro@mail.ru>
2023-09-11 00:07:28 +0300

    5 lab

--- first_lab/first_lab/urls.py
+++ first_lab/first_lab/urls.py
@@ -1,10 +1,8 @@
-from pages import views
-from posts import views
+from blog import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.HomePageView.as_view(), name = 'home'),
-    path('about/',views.index_about, name ='about'),
+    path('',views.BlogListView.as_view(), name = 'home'),
 ]
