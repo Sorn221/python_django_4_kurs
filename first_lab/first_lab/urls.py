00a584985fcf5b280bd12a5d4eb7ba5f0c5b04d3
razewithlove <mister.timkinbro@mail.ru>
2023-09-10 21:37:50 +0300

    4 lab

--- first_lab/first_lab/urls.py
+++ first_lab/first_lab/urls.py
@@ -1,9 +1,10 @@
 from pages import views
+from posts import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index, name = 'home'),
+    path('',views.HomePageView.as_view(), name = 'home'),
     path('about/',views.index_about, name ='about'),
 ]
--- first_lab/pages/views.py
+++ first_lab/pages/views.py
@@ -4,5 +4,4 @@ from django.http import HttpResponse
 def index(request):
     return render(request, "home.html")
 
-def index_about(request):
-    return render(request,"about.html")
+
